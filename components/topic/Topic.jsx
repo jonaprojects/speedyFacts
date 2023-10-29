58992009e06bfcf789a49bf31af3364ae0bfca11
jonaprojects <merkava234@gmail.com>
2023-10-29 02:28:40 +0200

    Fixed a bug of clicking topics

--- components/topic/Topic.jsx
+++ components/topic/Topic.jsx
@@ -8,10 +8,12 @@ const openSans = Open_Sans({ subsets: ["latin"] });
 
 export default function Topic(props) {
   return (
-    <div className="px-3 py-7 md:py-10 lg:py-12 bg-white transition duration-300 ease-in-out hover:cursor-pointer shadow-lg rounded-lg hover:bg-sky-500 hover:ring-sky-500 hover:text-white">
+    <div
+      className="px-3 py-7 md:py-10 lg:py-12 bg-white transition duration-300 ease-in-out hover:cursor-pointer shadow-lg rounded-lg hover:bg-sky-500 hover:ring-sky-500 hover:text-white"
+      onClick={props?.onClick}
+    >
       <div
         className={`w-full flex justify-center items-center text-center ${inter.className} text-lg md:text-2xl`}
-        onClick={props?.onClick}
       >
         {props.name}
       </div>
