d9e36e7b99cb267e9f464dee9db6c201cecba12f
jonaprojects <merkava234@gmail.com>
2023-11-19 00:45:25 +0200

    Changed gooter from copyrights and name to donating money to the FIDF

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -9,10 +9,24 @@ const openSans = Open_Sans({ subsets: ["latin"] });
 export default function Footer(props) {
   return (
     <footer
-      className={`w-full ${openSans.className} bg-slate-600 text-white py-3 px-2 fixed bottom-0`}
+      className={`w-full ${openSans.className} bg-blue-700 text-white py-1 px-2 fixed bottom-0`}
     >
       <ul className="flex">
-        <li className="">  © All rights reserved to Jonathan S.  </li>
+        <li className="hover:bg-blue-800 p-2">
+          <a href="https://www.fidf.org/" className="font-bold">
+            <div className="flex gap-3">
+              <p> ISRAEL AT WAR: Donate to FIDF Here</p>
+              <Image
+                src="/israel_flag.svg"
+                alt=""
+                className=""
+                width={24}
+                height={24}
+                priority
+              />
+            </div>
+          </a>
+        </li>
       </ul>
     </footer>
   );
